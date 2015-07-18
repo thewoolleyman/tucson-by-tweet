d825ae2d234dc561600d22c630771f7277483b9a
Chad <thewoolleyman@gmail.com>
2015-07-18 16:38:02 -0700

    alternate creds

--- lib/tweet.rb
+++ lib/tweet.rb
@@ -4,10 +4,17 @@ require 'json'
 class Tweet
   def initialize
     @client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = "6g0sHM9mPApQW2TaNGWWLK0H7"
-      config.consumer_secret     = "5gdi8hHFHzov7MLgcxSJEhzt7I76vHnylOy67tuzwnFsMARBht"
-      config.access_token        = "5746962-MUBBH5pzAhuVTdEnHdsjbPB9MY31m9z5UJw4r0nbhg"
-      config.access_token_secret = "6faj6bQJh3odooEJQrTC9VRXviHjS1a6xYev1ytAAJqqg"
+      # tucson-by-tweet
+      # config.consumer_key        = "6g0sHM9mPApQW2TaNGWWLK0H7"
+      # config.consumer_secret     = "5gdi8hHFHzov7MLgcxSJEhzt7I76vHnylOy67tuzwnFsMARBht"
+      # config.access_token        = "5746962-MUBBH5pzAhuVTdEnHdsjbPB9MY31m9z5UJw4r0nbhg"
+      # config.access_token_secret = "6faj6bQJh3odooEJQrTC9VRXviHjS1a6xYev1ytAAJqqg"
+
+      # tucson-by-tweet-2
+      config.consumer_key        = "nYCMbcHvDwjLIElkp0q4FBR4v"
+      config.consumer_secret     = "4DjK8y3AYTLLYTru1ckSQQBtuX1mMuqGSByDcAydqZXWEqrsy6"
+      config.access_token        = "5746962-uKEMtWIYA950QxxA0RPijzLx3QuWDWEUYEQVRaCkLm"
+      config.access_token_secret = "7YBkOvtvhbgTGOk7D3bMchaq3M35bxn6QCyxxAQXH0v9b"
     end
   end
 
