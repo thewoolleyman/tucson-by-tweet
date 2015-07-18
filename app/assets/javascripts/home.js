df58f42157435029a5b501b5e572bd0c89d8f6ee
Dennis Hoer <dennis.hoer@pearson.com>
2015-07-18 13:53:56 -0700

    Add css js hooks

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,2 +1,9 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+function content() {
+  $('#content').html('this is the content');
+}
+
+$(document).ready(content);
+
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
 
-  # GET /homes/1
-  # GET /homes/1.json
-  def show
-    render text: 'tucson by tweet'
-  end
+  # # GET /homes/1
+  # # GET /homes/1.json
+  # def show
+  #   render text: 'tucson by tweet'
+  # end
 end
